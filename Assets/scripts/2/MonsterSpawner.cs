a82ab05ca5f81e5bc4ad0a4cb3e52406e391dcf1
PreetRajput <rajputpreet72@gmail.com>
2024-09-05 18:32:12 +0530

    changes

--- Assets/scripts/2/MonsterSpawner.cs
+++ Assets/scripts/2/MonsterSpawner.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
 public class MonsterSpawner : MonoBehaviour
 {
     [SerializeField]
@@ -42,24 +44,22 @@ public class MonsterSpawner : MonoBehaviour
         {
         yield return new WaitForSeconds(Random.Range(1,5));
         monsIndex= Random.Range(0, allEnemies.Length);
-        spawnedMonster= Instantiate(allEnemies[monsIndex]);
-        sr= spawnedMonster.GetComponent<SpriteRenderer>();
 
         side= Random.Range(0,2);
         //0 ==> left
         if (side==0)
         {
-          spawnedMonster.transform.position= leftpos.transform.position;
+          spawnedMonster= PhotonNetwork.Instantiate(allEnemies[monsIndex].name, leftpos.transform.position, Quaternion.identity);
           spawnedMonster.GetComponent<Monsters>().speed= Random.Range(1,10);
+          sr= spawnedMonster.GetComponent<SpriteRenderer>();
           sr.flipX= false;
         }
         else //1 ==> right 
         {
-          spawnedMonster.transform.position= rightpos.transform.position;
+          spawnedMonster= PhotonNetwork.Instantiate(allEnemies[monsIndex].name, rightpos.transform.position, Quaternion.identity);
           spawnedMonster.GetComponent<Monsters>().speed= -Random.Range(1,10);
+          sr= spawnedMonster.GetComponent<SpriteRenderer>();
           sr.flipX= true;
-          
-
         }
             
         } // while 
